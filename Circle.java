9e13036a87100a3150a635d573f0d22cba46380c
Ashfaq463 <sashfaq463@gmail.com>
2023-01-07 19:43:22 +0530

    Display Method Deleted.

--- Circle.java
+++ Circle.java
@@ -5,11 +5,8 @@ class Circle extends Shape {
 	public void compute_area()
 	{
 		area = 3.142 * num1 * num1;
-	}
-	public void display1()
-	{
+
 		System.out.println("The area of circle is : "+area);
 	}
-	
 
 }

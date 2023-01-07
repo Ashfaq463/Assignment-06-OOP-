350cf41efff8ef0b41e3a5f2f754c4fbe2fdbd93
Ashfaq463 <sashfaq463@gmail.com>
2023-01-07 19:59:22 +0530

    Value of pi updated.

--- Circle.java
+++ Circle.java
@@ -4,7 +4,7 @@ class Circle extends Shape {
 	
 	public void compute_area()
 	{
-		area = 3.142 * num1 * num1;
+		area = 3.142342 * num1 * num1;
 
 		System.out.println("The area of circle is : "+area);
 	}

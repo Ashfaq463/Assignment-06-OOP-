b3a6bdbeb780e16d2056e0236e022a10a76a5300
Ashfaq463 <sashfaq463@gmail.com>
2023-01-08 12:04:58 +0530

    Saving Changes.

--- Circle.java
+++ Circle.java
@@ -1,10 +1,9 @@
 
 class Circle extends Shape {
-	double area;
 	
 	public void compute_area()
 	{
-		area = 3.142342 * num1 * num1;
+		double area = 3.142342 * num1 * num1;
 
 		System.out.println("The area of circle is : "+area);
 	}
